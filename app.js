f1b9a78bab990347ee2512c3066ce361c499eca0
ElizaIsADev <elizaisadev@gmail.com>
2024-09-18 16:02:04 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,40 @@
 function pesquisar(){
-    //console.log(dados);
 
-    let section = document.getElementById("resultados-pesquisa")
-    
-    let resultados = ""
-    
-    //para cada dado dentro de uma lista de dados
-    for(let dado of dados) {
-        resultados += `
-        <div class="item-resultado">
-        <h2><a href="#"target="_blank">${dado.titulo}</a></h2>
-        <p class="descricao-meta">${dado.descricao}</p>
-    </div>`
+    let section = document.getElementById("resultados-pesquisa");
+
+    let campoPesquisa = document.getElementById("campo-pesquisa").value
+
+    if (!campoPesquisa){
+        section.innerHTML = "<p>Nada foi encontrado :/</p>"
+        return
     }
     
+    campoPesquisa = campoPesquisa.toLowerCase()
+
+    let resultados = "";
+    let titulo = "";
+    let descricao = "";
+    let tags = "";
+    
+    for(let dado of dados) {
+        titulo = dado.titulo.toLowerCase()
+        descricao = dado.descricao.toLowerCase()
+        tags = dado.tags.toLowerCase()
+
+        if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)){
+            resultados += `
+            <div class="item-resultado">
+            <h2><a href="#"target="_blank">${dado.titulo}</a></h2>
+            <p class="descricao-meta">${dado.descricao}</p>
+            </div>
+            `;
+         }
+    }
+    
+    if (!resultados){
+        resultados ="<p>Nada foi encontrado</p>"
+    } 
+
     section.innerHTML = resultados
     
 }
